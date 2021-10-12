2613510f8ced141be173ed02cde5f3715a96ec80
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-12 16:03:53 -0500

    fixed typo homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={HomePage} />
+        <Route exact path='/' component={Homepage} />
       </Switch>
     </div>
   );
