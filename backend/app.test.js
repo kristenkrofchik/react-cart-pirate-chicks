c7cc996c2f02e89681e591a07947047f995e929d
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-26 21:00:10 -0500

    some backend testing

---
+++ backend/app.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+
+const app = require("./app");
+const db = require("./db");
+
+test("not found for site 404", async function () {
+    const resp = await request(app).get("/does-not-exist");
+    expect(resp.statusCode).toEqual(404);
+  });
+
+afterAll(function () {
+    db.end();
+});
