17055d4a0006bfeda4046818932606ea4ebfce87
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-28 12:35:23 -0500

    fixing it

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = require('./app');
 const { PORT } = require('./config');
 
 app.listen(PORT, function() {
-    console.log(`Started on http://localhost:${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
 
