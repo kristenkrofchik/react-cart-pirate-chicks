210403f78f38591afca8566c5039024adaed940e
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-28 20:04:41 -0500

    attempt heroku

--- server.js
+++ server.js
@@ -3,7 +3,15 @@
 'use strict'
 
 const app = require('./app');
+const path = require('path');
 const { PORT } = require('./config');
+
+app.use(express.static(path.join(__dirname, './frontend/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './frontend/build', 'index.js'));
+});
+
 
 app.listen(PORT, function() {
     console.log(`Server started on port ${PORT}`);
