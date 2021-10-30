fd5f845d206d62a0402ab46ad25500af150eb523
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-30 08:38:33 -0500

    frontend deploy

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require('path');
 const express = require('express');
 const { PORT } = require('./config');
 
-app.use(express.static(path.join(__dirname, './frontend/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
