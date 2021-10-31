3fe8833afa2554dbf29a182e8496a6265399af7b
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-30 21:14:00 -0500

    small styling and trying to connect backend;

--- frontend/src/components/shared/LoadingSpinner.jsx
+++ frontend/src/components/shared/LoadingSpinner.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Layout from './Layout';
 import './LoadingSpinner.styles.css';
 
 /** Loading message used by components that fetch API data. */
 
 function LoadingSpinner() {
   return (
+    <Layout>
       <div className="LoadingSpinner">
         Loading ...
       </div>
+    </Layout>
   );
 }
 
